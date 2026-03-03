fea7725d60daed0cf4cb14239fc7051b6f9f976b
zhaochenyang20 <zhaochen20@outlook.com>
2026-03-03 05:35:17 +0000

    move RL related tests to post-training dir
    (cherry picked from commit 3102e77dc8021fca6cc6def139628e643d1b3a39)

--- python/sglang/multimodal_gen/test/run_suite.py
+++ python/sglang/multimodal_gen/test/run_suite.py
@@ -21,7 +21,9 @@ from sglang.multimodal_gen.runtime.utils.logging_utils import init_logger
 
 logger = init_logger(__name__)
 
-_UPDATE_WEIGHTS_FROM_DISK_TEST_FILE = "test_update_weights_from_disk.py"
+_UPDATE_WEIGHTS_FROM_DISK_TEST_FILE = (
+    "../post_training/test_update_weights_from_disk.py"
+)
 _UPDATE_WEIGHTS_MODEL_PAIR_ENV = "SGLANG_MMGEN_UPDATE_WEIGHTS_PAIR"
 _UPDATE_WEIGHTS_MODEL_PAIR_IDS = (
     "FLUX.2-klein-base-4B",
@@ -30,9 +32,9 @@ _UPDATE_WEIGHTS_MODEL_PAIR_IDS = (
 
 SUITES = {
     "1-gpu": [
-        # RL tests
-        "test_e2e_sleep_wake_up.py",
-        "test_update_weights_from_disk.py",
+        # RL / post-training tests
+        "../post_training/test_e2e_sleep_wake_up.py",
+        "../post_training/test_update_weights_from_disk.py",
         # Server tests
         "test_server_a.py",
         "test_server_b.py",
